67b0d9d1fe57a4a85e866b23a5fb5dc5dddc74f3
235005 <235005@KB37-303-C9>
2025-10-22 16:14:41 +0300

    Добавьте файлы проекта.

---
+++ Pr777/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr777
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
